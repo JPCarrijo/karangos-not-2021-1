052a23eed60c49b53598da7f3d7bb1d4a3882588
JPCarrijo <carrijojp@gmail.com>
2021-04-28 01:23:27 +0000

    (27/04) Correção da PROVA 1 e ínicio do Projeto Karangos

---
+++ karangos/src/App.js
@@ -0,0 +1,42 @@
+//import logo from './logo.svg';
+//import './App.css';
+//import Button from '@material-ui/core/Button'
+import TopBar from './ui/TopBar'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import yellow from '@material-ui/core/colors/yellow';
+import pink from '@material-ui/core/colors/pink';
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: yellow[500],
+    },
+    secondary: {
+      main: pink[500],
+    },
+  },
+});
+
+function App() {
+  return (
+    /*<div className="App">
+      <header className="App-header">
+        <h1>Projeto Karangos</h1>
+        <Button variant="contained" color="primary">Click Aqui</Button>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
+      </header>
+    </div>*/
+    <div>
+      <ThemeProvider theme={theme}>
+        <TopBar/>
+      </ThemeProvider>
+    </div>
+  );
+}
+
+export default App;
